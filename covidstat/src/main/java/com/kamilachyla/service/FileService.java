5cfe4caef12a60df180552feeb7be5bc7bb15d1c
Kamila Chyla <kamila.chyla@gmail.com>
2021-04-12 17:28:28 +0200

    Remove System.out debug messages

--- covidstat/src/main/java/com/kamilachyla/service/FileService.java
+++ covidstat/src/main/java/com/kamilachyla/service/FileService.java
@@ -7,7 +7,7 @@ import com.kamilachyla.deserialize.CaseDeserializer;
 import com.kamilachyla.model.Case;
 import com.kamilachyla.model.Country;
 
-import java.io.*;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
@@ -19,23 +19,19 @@ public class FileService implements CovidService {
     public FileService() {
         mapper.configure(
                 DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
         SimpleModule module = new SimpleModule();
         module.addDeserializer(Case.class, new CaseDeserializer());
         mapper.registerModule(module);
     }
     @Override
     public Stream<Country> getCountries() {
-        System.out.printf("GetCountries%n");
         return deserialize(COUNTRIES_FILE, Country.class);
     }
 
     private <T> Stream<T> deserialize(String fname, Class<T> tClass) {
         Stream<T> result;
-        System.out.printf("in deserialize %s%n", fname);
         try(InputStream is = Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(fname))){
             List<T> values = mapper.readerForListOf(tClass).readValue(is);
-            System.out.printf("Read from %s: %n%s%n", fname, values.isEmpty() ? "Empty": values.get(0));
             result = values.stream();
         } catch (Exception e) {
             result = Stream.empty();
@@ -46,7 +42,6 @@ public class FileService implements CovidService {
 
     @Override
     public Stream<Case> getCases(Country data) {
-        System.out.printf("GetCases for %s%n", data);
         return deserialize(data.slug() + ".json", Case.class);
     }
 }
